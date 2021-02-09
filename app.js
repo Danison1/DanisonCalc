765d6f19112ec97e9f6e3dcea19c36ae587e0c53
Danison1 <66676493+Danison1@users.noreply.github.com>
2021-02-09 13:28:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const input = document.querySelector('.input')
 const select = document.querySelector('.select1')
 const answer = document.querySelector('.answer')
+const botsTotal = document.querySelector('.botsTotal')
+const betsTotal = document.querySelector('.betsTotal')
 
 const arr2 = []
 
 select.addEventListener('change', (event) => {
   const selectValue = Number(event.target.value)
   const value = Number(input.value)
+
   if (selectValue === 100) {
     let num = value * selectValue - 100
     arr2.push(num)
@@ -39,13 +42,12 @@ select.addEventListener('change', (event) => {
   }
 
   let sum = 0
-
+  
   for(let i = 0; i < arr2.length; i ++) {
     sum += arr2[i]
   }
   answer.innerHTML = Math.floor(sum)
+  botsTotal.innerHTML = sum / 500
+  betsTotal.innerHTML = arr2.length
 })
-
-
-
 
